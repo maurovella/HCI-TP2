c9320948c93c0e36b8f8d6b788499b94aca34719
santiagohirsch <shirsch@itba.edu.ar>
2023-04-29 17:40:02 -0300

    Home page initial advancements

--- SmartLiving/src/App.vue
+++ SmartLiving/src/App.vue
@@ -1,47 +1,97 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+import 'vuetify/styles'
+
+const selection = ref([]);
+const items = [
+    {
+        src: 'backgrounds/bg.jpg',
+    },
+    {
+        src: 'backgrounds/md.jpg',
+    },
+    {
+        src: 'backgrounds/bg-2.jpg',
+    },
+    {
+        src: 'backgrounds/md2.jpg',
+    },
+];
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <header>
+        <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125"/>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+        <div class="wrapper">
+            <HelloWorld msg="SmartLiving" />
+        </div>
+    </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+        <v-item-group mandatory>
+            <v-container>
+                <v-row>
+                    <v-col
+                        v-for="n in 3"
+                        :key="n"
+                        cols="12"
+                        md="4"
+                    >
+                        <v-item v-slot="{ isSelected, toggle }">
+                            <v-card
+                                :color="isSelected ? 'primary' : ''"
+                                class="d-flex align-center"
+                                dark
+                                height="200"
+                                @click="toggle"
+                                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+                            >
+                                <v-scroll-y-transition>
+                                    <div
+                                        class="text-h3 flex-grow-1 text-center"
+                                    >
+                                        {{ isSelected ? 'Ja PT' : 'Click Me!' }}
+                                    </div>
+                                </v-scroll-y-transition>
+                            </v-card>
+                        </v-item>
+                        <div class="text-h5 flex-grow-1 text-center">
+                            Nombre Residencia
+                        </div>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-item-group>
+    </main>
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
+    line-height: 1.5;
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+    display: block;
+    margin: 0 auto 2rem;
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+    header {
+        display: flex;
+        place-items: center;
+        padding-right: calc(var(--section-gap) / 2);
+    }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+    .logo {
+        margin: 0 2rem 0 0;
+    }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+    header .wrapper {
+        display: flex;
+        place-items: flex-start;
+        flex-wrap: wrap;
+    }
 }
 </style>
--- SmartLiving/src/main.js
+++ SmartLiving/src/main.js
@@ -1,6 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-import './assets/main.css'
+const app = createApp(App)
+const vuetify = createVuetify({
+    components,
+    directives
+})
 
-createApp(App).mount('#app')
+app.use(vuetify)
+app.mount('#app')
