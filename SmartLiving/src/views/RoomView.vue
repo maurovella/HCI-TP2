a4a932691ab6ffa33400c68e81a52deb22eec6c8
santiagohirsch <shirsch@itba.edu.ar>
2023-05-06 17:10:27 -0300

    Added roomview file

---
+++ SmartLiving/src/views/RoomView.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-container>
+        <h1>Armar room view</h1>
+    </v-container>
+</template>
+
+<style >
+body{
+    background-color: #4c4592;
+}
+header {
+    line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+    header {
+        display: flex;
+        place-items: center;
+        padding-right: calc(var(--section-gap) / 2);
+    }
+}
+</style>
+<script setup>
+</script>
