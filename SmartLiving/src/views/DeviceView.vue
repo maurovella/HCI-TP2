3a39197fb82f02014d1b1d614f20789e56a0c8f1
maurov <mvella@itba.edu.ar>
2023-05-08 12:24:57 -0300

    Devices View

---
+++ SmartLiving/src/views/DeviceView.vue
@@ -0,0 +1,66 @@
+<script>
+import DeviceCard from "@/components/DeviceCard.vue"
+import Title from "@/components/Title.vue";
+
+export default {
+    components: {Title, DeviceCard},
+
+}
+</script>
+
+<template>
+    <Title text="Dispositivos"/>
+    <v-sheet class="mx-sm-2 my-sm-5" border rounded color="primary">
+        <v-slide-group show-arrows>
+            <v-slide-group-item>
+                <DeviceCard/>
+            </v-slide-group-item>
+            <v-slide-group-item>
+                <DeviceCard/>
+            </v-slide-group-item>
+            <v-slide-group-item>
+                <DeviceCard/>
+            </v-slide-group-item>
+            <v-slide-group-item>
+                <DeviceCard/>
+            </v-slide-group-item>
+            <v-slide-group-item>
+                <DeviceCard/>
+            </v-slide-group-item>
+            <v-slide-group-item>
+                <DeviceCard/>
+            </v-slide-group-item>
+            <v-slide-group-item>
+                <DeviceCard/>
+            </v-slide-group-item>
+            <v-slide-group-item>
+                <DeviceCard/>
+            </v-slide-group-item>
+            <v-slide-group-item>
+                <DeviceCard/>
+            </v-slide-group-item>
+            <v-slide-group-item>
+                <DeviceCard/>
+            </v-slide-group-item>
+            <v-slide-group-item>
+                <DeviceCard/>
+            </v-slide-group-item>
+            <v-slide-group-item>
+                <DeviceCard/>
+            </v-slide-group-item>
+            <v-slide-group-item>
+                <DeviceCard/>
+            </v-slide-group-item>
+            <v-slide-group-item>
+                <DeviceCard/>
+            </v-slide-group-item>
+            <v-slide-group-item>
+                <DeviceCard/>
+            </v-slide-group-item>
+            <v-slide-group-item>
+                <DeviceCard/>
+            </v-slide-group-item>
+
+        </v-slide-group>
+    </v-sheet>
+</template>
